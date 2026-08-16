8b93cea572083939e38d08006deea544718d51e1
Anureddy7956 <4q4947@gmail.com>
2026-08-16 10:30:13 +0530

    event-handeling in react

---
+++ event-handling/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+//import ProductTab from "./ProductTab.jsx";
+import Button from "./Button.jsx";
+function App() {
+  return(
+    <>
+    <Button/>
+    </>
+  );
+}
+
+export default App;

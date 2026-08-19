118e29e0ad401ad9ca000037ba6ff7a1091850c4
Anureddy7956 <4q4947@gmail.com>
2026-08-19 12:45:26 +0530

    Update react part 3

--- Activity/MsgBox.jsx
+++
@@ -1,9 +0,0 @@
-/* Show a Hello Message to the user in the diffrent colors.
-Pass 2 values as props : userName & textColor      */
-
-export default function MsgBox({userName,textColor}){
-    return (
-        <h1 style={{color:textColor}}>Hello,{userName}</h1>
-    );
-
-}
--- import-export/Title.jsx
+++
@@ -1,6 +0,0 @@
-function Title(){
-  return <h1>I'm mother fucker</h1>;
-}
-
-// export default Title; - default export
-export {Title}; 
